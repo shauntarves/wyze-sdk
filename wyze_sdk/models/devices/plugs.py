851c71504154057e38fdab9833b04dd91c37333d
Shaun Tarves <shaun@tarves.net>
2022-11-08 18:23:55 -0500

    Initial support for plug usage data

--- wyze_sdk/models/devices/plugs.py
+++ wyze_sdk/models/devices/plugs.py
@@ -1,8 +1,11 @@
 from __future__ import annotations
+from datetime import datetime
+import json
+import logging
 
-from typing import Optional, Set, Union
+from typing import Optional, Sequence, Set, Tuple, Union
 
-from wyze_sdk.models import PropDef, show_unknown_key_warning
+from wyze_sdk.models import JsonObject, PropDef, epoch_to_datetime, show_unknown_key_warning
 from wyze_sdk.models.devices import (AbstractWirelessNetworkedDevice,
                                      DeviceProp, DeviceProps, SwitchableMixin)
 
@@ -27,6 +30,60 @@ class PlugProps(object):
     @classmethod
     def photosensitive_switch(cls) -> PropDef:
         return PropDef("photosensitive_switch", bool)
+
+
+class PlugUsageRecord(JsonObject):
+    """
+    A plug usage record.
+    """
+
+    @property
+    def attributes(self) -> Set[str]:
+        return {
+            "date",
+            "hourly_data",
+        }
+
+    logger = logging.getLogger(__name__)
+
+    def __init__(
+        self,
+        *,
+        date: datetime = None,
+        hourly_data: Sequence[int] = None,
+        **others: dict
+    ):
+        if date is None:
+            date = self._extract_attribute('date_ts', others)
+        if isinstance(date, int):
+            date = epoch_to_datetime(date, ms=True)
+        self.date = date
+        if hourly_data is not None:
+            self.hourly_data = hourly_data
+        else:
+            hourly_data = self._extract_attribute('data', others)
+            if isinstance(hourly_data, str):
+                hourly_data = json.loads(hourly_data)
+            if not isinstance(hourly_data, (list, Tuple)):
+                hourly_data = list(hourly_data)
+            self.hourly_data = []
+            for _data in hourly_data:
+                if isinstance(_data, int):
+                    self.hourly_data.append(_data)
+                else:
+                    try:
+                        self.hourly_data.append(int(_data))
+                    except ValueError:
+                        self.logger.warning(f"invalid usage record data '{_data}'")
+                        self.hourly_data.append(0)
+        show_unknown_key_warning(self, others)
+
+    @property
+    def total_usage(self) -> Optional[float]:
+        """
+        Return the total usage, in kWh, for the day.
+        """
+        return None if self.hourly_data is None else sum(self.hourly_data) / 1000.0
 
 
 class Plug(SwitchableMixin, AbstractWirelessNetworkedDevice):
